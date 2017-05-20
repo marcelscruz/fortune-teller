daf926d88fcdd25b6fd9de2ac8ba84ac4ad9b05e
Marcel Cruz <marcelcruz@live.com>
2017-05-20 19:37:21 +0100

    minor fixes

--- modal.js
+++ modal.js
@@ -7,24 +7,30 @@ var btn = document.getElementById("about");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("x-button")[0];
 
+// Get the page section to make the input always on focus
+var pageSection = document.getElementById('page-section');
+
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-//    modal.style.display = "block";
 	$(".modal").fadeIn(300);
+	count2 = 1; 
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-//    modal.style.display = "none";
 	$(".modal").fadeOut(300);
 	insert1.focus();
 }
 
 // When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere in the page keeps focus on input1 or input2
 window.onclick = function(event) {
     if (event.target == modal) {
-//        modal.style.display = "none";
 		$(".modal").fadeOut(300);
 		insert1.focus();
+    } else if (event.target == pageSection && count == 0 || count == 1) {
+		insert1.focus();
+    } else if (event.target == pageSection && count == 2) {
+		insert2.focus();
     }
 }
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var insert1 = document.getElementById('insert1');
 var insert2 = document.getElementById('insert2');
+var inputBox = '';
+var answer = document.getElementById('answer');
 var noAnswer;
 var questions;
 var madeBy = document.getElementById('made_by');
@@ -11,7 +13,8 @@ var pt = document.getElementById('pt');
 window.onpaint = english();
 
 
-/* ENGLISH  */ 
+// ENGLISH   
+
 function english() {
 
 	questions = ['Miss, could you tell me what is ', 'Dear, I\'d like know what is ', 'Please, could you tell me what is '];
@@ -27,10 +30,13 @@ function english() {
 	$("#pt").removeClass("active");
 	$("#en").addClass("active");
 	insert1.focus();
+	reset();
 	
 };
 
-/* PORTUGUESE */
+
+// PORTUGUESE 
+
 function portuguese() {
 
 	questions = ['A senhora poderia me dizer qual é ', 'Senhora vidente, poderia me dizer qual é ', 'Por favor, poderia me dizer qual é '];
@@ -46,13 +52,15 @@ function portuguese() {
 	$("#en").removeClass("active");
 	$("#pt").addClass("active");
 	insert1.focus();
+	reset();
 	
 };
 
 
-// LISTENS KEY PRESS
+// LISTENS KEY PRESS 
 
-var count = 0;
+var count = 0; // NUMBERING START AND FINISH TO DECIDE FOR INPUT BOX
+var count2 = 0;	// CHECKING IF MODAL IS ON OR OFF
 
 document.onkeydown = function (evt) {
 
@@ -61,35 +69,40 @@ document.onkeydown = function (evt) {
 	if (evt.keyCode == 9 && count == 0) {
 		evt.preventDefault();
 		start();
-		count = count + 1;
 	} else if (evt.keyCode == 9 && count == 1) {
 		evt.preventDefault();
 		finish();
 	} else if (evt.keyCode == 13) {
 		ask();
-	} else if (evt.keyCode == 27) {
+	} else if (evt.keyCode == 27 && count2 == 0) {
+		reset();
+	} else if (evt.keyCode == 27 && count2 == 1) {
 		$(".modal").fadeOut(300);
 		insert1.focus();
+		count2 = 0;
 	}
 };
-
 
 // STARTS RECORDING
 function start() {
 
 	insert1.style.opacity = 0;
-
+	
 	insert2.style.opacity = 1;
 	insert2.style.zIndex = 2;
+	
+	count = 1;
 
 }
 
 
 // FINISHES RECORDING
 function finish() {
+	
 	insert2.focus();
 	inputBox = document.getElementById('insert1').value;
-
+	
+	count = 2;
 };
 
 
@@ -99,16 +112,37 @@ function ask() {
 	if (count == 0) {
 
 		
-		document.getElementById('answer').innerHTML = noAnswer;
-		document.getElementById('answer').style.marginBottom = '12%';
+		answer.innerHTML = noAnswer;
+		answer.style.marginBottom = '12%';
 		
-		$("#answer").fadeIn(4000);
+		$("#answer").fadeIn(2000);
 
-	} else if (count == 1) {
+	} else if (count == 2) {
 		document.getElementById('answer').innerHTML = inputBox;
 
-		$("#answer").fadeIn(4000);
+		$("#answer").fadeIn(2000);
 	}
+}
+
+// RESET
+
+function reset() {
+	insert1.value = '';
+	insert1.focus();
+	insert1.style.opacity = 1;
+	
+	insert2.value = '';
+	insert2.style.opacity = 0;
+	insert2.style.zIndex = 0;
+	
+	inputBox = '';
+	
+	answer.value = '';
+	answer.style.display = 'none';
+	answer.style.marginBottom = '15%';
+	
+	index = 0;
+	count = 0;
 }
 
 

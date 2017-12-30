caabe2f0b1a6b51f80469cb3757ff98693e6be30
Marcel Cruz <marcelcruz@live.com>
2017-12-30 18:56:47 +0000

    minor changes to js

--- modal.js
+++ modal.js
@@ -7,30 +7,29 @@ var btn = document.getElementById("about");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("x-button")[0];
 
-// Get the page section to make the input always on focus
-var pageSection = document.getElementById('page-section');
+var modalOpened = false;
 
-// When the user clicks on the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
+//    modal.style.display = "block";
 	$(".modal").fadeIn(300);
-	count2 = 1; 
+	modalOpened = true;
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+//    modal.style.display = "none";
 	$(".modal").fadeOut(300);
 	insert1.focus();
+	modalOpened = false;
 }
 
 // When the user clicks anywhere outside of the modal, close it
-// When the user clicks anywhere in the page keeps focus on input1 or input2
 window.onclick = function(event) {
     if (event.target == modal) {
+//        modal.style.display = "none";
 		$(".modal").fadeOut(300);
 		insert1.focus();
-    } else if (event.target == pageSection && count == 0 || count == 1) {
-		insert1.focus();
-    } else if (event.target == pageSection && count == 2) {
-		insert2.focus();
+		modalOpened = false;
     }
 }
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var insert1 = document.getElementById('insert1');
 var insert2 = document.getElementById('insert2');
-var inputBox = '';
-var answer = document.getElementById('answer');
 var noAnswer;
 var questions;
 var madeBy = document.getElementById('made_by');
@@ -13,96 +11,89 @@ var pt = document.getElementById('pt');
 window.onpaint = english();
 
 
-// ENGLISH   
-
+/* ENGLISH  */
 function english() {
 
 	questions = ['Miss, could you tell me what is ', 'Dear, I\'d like know what is ', 'Please, could you tell me what is '];
-	
+
 	noAnswer = 'You should be more polite';
-	
+
 	madeBy.innerHTML = 'made by ';
-	
+
 	help.innerHTML = "Press TAB to start typing the <span class='underline'>answer</span>. It won't appear on the screen, you'll actually see the beginning of the q<span class='underline'>uestion</span> being written for you while you type.<br><br>Press TAB again once the <span class='underline'>answer</span> is ready, so you can finish typing the q<span class='underline'>uestion</span> by yourself.";
-	
+
 	instr.innerHTML = 'Type your question and press ENTER to ask or ESC to reset';
-	
+
 	$("#pt").removeClass("active");
 	$("#en").addClass("active");
 	insert1.focus();
-	reset();
-	
+
 };
 
-
-// PORTUGUESE 
-
+/* PORTUGUESE */
 function portuguese() {
 
 	questions = ['A senhora poderia me dizer qual é ', 'Senhora vidente, poderia me dizer qual é ', 'Por favor, poderia me dizer qual é '];
-	
+
 	noAnswer = 'Você deveria ser mais educado';
-	
+
 	madeBy.innerHTML = 'feito por ';
-	
+
 	help.innerHTML = "Aperte TAB para começar a digitar a <span class='underline'>res</span>p<span class='underline'>osta</span>. Ela não vai aparecer na tela, você vai na verdade ver a p<span class='underline'>er</span>g<span class='underline'>unta</span> sendo escrita enquanto você digita.<br><br>Aperte TAB de novo quando a <span class='underline'>res</span>p<span class='underline'>osta</span> estiver pronta, então você pode terminar de digitar a p<span class='underline'>er</span>g<span class='underline'>unta</span> por si mesmo.";
-	
+
 	instr.innerHTML = 'Escreva sua pergunta e aperte ENTER para perguntar ou ESC para reiniciar';
-	
+
 	$("#en").removeClass("active");
 	$("#pt").addClass("active");
 	insert1.focus();
-	reset();
-	
+
 };
 
 
-// LISTENS KEY PRESS 
+// LISTENS KEY PRESS
 
-var count = 0; // NUMBERING START AND FINISH TO DECIDE FOR INPUT BOX
-var count2 = 0;	// CHECKING IF MODAL IS ON OR OFF
+var count = 0;
 
 document.onkeydown = function (evt) {
 
 	evt = evt || window.event;
 
+	// console.log(evt.keyCode);
+
 	if (evt.keyCode == 9 && count == 0) {
 		evt.preventDefault();
 		start();
+		count++;
 	} else if (evt.keyCode == 9 && count == 1) {
 		evt.preventDefault();
 		finish();
 	} else if (evt.keyCode == 13) {
 		ask();
-	} else if (evt.keyCode == 27 && count2 == 0) {
-		reset();
-	} else if (evt.keyCode == 27 && count2 == 1) {
+	} else if (evt.keyCode == 27 && modalOpened == true) {
 		$(".modal").fadeOut(300);
 		insert1.focus();
-		count2 = 0;
+	} else if (evt.keyCode == 27 && modalOpened == false) {
+		document.location.reload();
 	}
 };
+
 
 // STARTS RECORDING
 function start() {
 
 	insert1.style.opacity = 0;
-	
+
 	insert2.style.opacity = 1;
 	insert2.style.zIndex = 2;
-	
-	count = 1;
 
 }
 
 
 // FINISHES RECORDING
 function finish() {
-	
 	insert2.focus();
 	inputBox = document.getElementById('insert1').value;
-	
-	count = 2;
+
 };
 
 
@@ -111,38 +102,17 @@ function ask() {
 
 	if (count == 0) {
 
-		
-		answer.innerHTML = noAnswer;
-		answer.style.marginBottom = '12%';
-		
-		$("#answer").fadeIn(2000);
 
-	} else if (count == 2) {
+		document.getElementById('answer').innerHTML = noAnswer;
+		document.getElementById('answer').style.marginBottom = '12%';
+
+		$("#answer").fadeIn(4000);
+
+	} else if (count == 1) {
 		document.getElementById('answer').innerHTML = inputBox;
 
-		$("#answer").fadeIn(2000);
+		$("#answer").fadeIn(4000);
 	}
-}
-
-// RESET
-
-function reset() {
-	insert1.value = '';
-	insert1.focus();
-	insert1.style.opacity = 1;
-	
-	insert2.value = '';
-	insert2.style.opacity = 0;
-	insert2.style.zIndex = 0;
-	
-	inputBox = '';
-	
-	answer.value = '';
-	answer.style.display = 'none';
-	answer.style.marginBottom = '15%';
-	
-	index = 0;
-	count = 0;
 }
 
 
